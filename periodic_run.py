1fb074caac8cee759dba0e6418a910178e020fb5
Александр Богданов <netolyrg@gmail.com>
2019-12-16 04:00:15 +0300

    change time

--- periodic_run.py
+++ periodic_run.py
@@ -10,7 +10,7 @@ from core import post_message
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('cron', hour=3)  # 4:00 UTC, 7:00 Moscow
+@scheduler.scheduled_job('cron', hour=4)  # 4:00 UTC, 7:00 Moscow
 def timed_job():
     post_message()
 
