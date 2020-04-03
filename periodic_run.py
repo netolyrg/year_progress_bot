faf637e2b2086c516e258aaf96036d9d6c6a7b71
Александр Богданов <netolyrg@gmail.com>
2020-04-03 20:25:27 +0300

    change time

--- periodic_run.py
+++ periodic_run.py
@@ -10,7 +10,7 @@ from core import post_message, is_right_day
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('cron', hour=4)  # 4:00 UTC, 7:00 Moscow
+@scheduler.scheduled_job('cron', hour=7)  # 7:00 UTC, 10:00 Moscow
 def timed_job():
     if is_right_day():
         post_message()
